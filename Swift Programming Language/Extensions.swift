e83c7e3c37711767aeb063e019d71aa0693bcfee
Paul Hughes <paul.hughes@hughesxploration.com>
2019-04-14 22:45:22 -0500

    Swift Programming Examples Uploaded

---
+++ Swift Programming Language/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  Slides
+//
+//  Created by Paul Hughes on 2/13/19.
+//  Copyright © 2019 Paul Hughes. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+        
+    }
+}
+
---
+++ Swift Programming Language/PhotoSelectorHeader.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoSelectorHeader.swift
+//  Slides
+//
+//  Created by Paul Hughes on 3/2/19.
+//  Copyright © 2019 Paul Hughes. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class PhotoSelectorHeader: UICollectionViewCell {
+    
+    let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+//        iv.backgroundColor = .cyan
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
